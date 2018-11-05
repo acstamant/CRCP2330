cdeb9dbb220f40800d820f401211f6e9d51f0a26
acstamant <astamant@smu.edu>
2018-11-04 21:56:56 -0600

    I dont think that I need to use sum, i think i can do it without it. The program is running but it is not multiplying the numbers, just keeps adding over and over again

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,31 +6,35 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-@sum 
- M=0
+
+@0
+D=M
+@2
+M=D
 
  (LOOP)
 
-	 @0
- 	D;JEQ
+ 	@0
+ 	D=M
+ 	D; JEQ
  	@1
- 	D;JEQ
+ 	D=M 
+ 	D; JEQ
+
+ 	@1
+ 	D=A
+ 	M=M-D
 
  	@0
  	D=M
- 	@1
+ 	@2
  	M=M+D
 
-	 @sum
-	D=M 
-
  	@LOOP
-	 0;JMP
+	0;JMP
 
  (END)
 
- @sum
- D=M
  @2
  M=D
 
