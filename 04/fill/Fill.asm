f3c680e719d26aed56f39fa1644b505917e539b5
acstamant <astamant@smu.edu>
2018-11-18 15:30:37 -0600

    Still working on fill...having some trouble getting it to increment to the next address to fill the entire screen

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,38 +12,39 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 @SCREEN
-D=A
+D=M
 @i
-M=D //initalizes screen and i to the same memory, i tells you where the next screen is 
+M=D
 
 (LOOP)
-	@24576
+	@KBD
 	D=M 
-	@BLACK 
-	D; JNE 
+	@BLACK
+	D; JGT
 	@WHITE
 	D; JEQ
 
-
 (BLACK)
-	@SCREEN
-	M=-1 
+	//@SCREEN
+	//M=-1
 
+	@SCREEN 
+	D=A
+	M=D+1
 	@i
-	M=A
-	D=M+1 //increments i to the next section of the screen 	
-	@LOOP
-	0; JMP
+	D=M 
 
 (WHITE)
 	@SCREEN 
 	M=0
 
-	@i 
-	M=A
-	D=M-1 //deincrements i so that the black goes away
+	@SCREEN 
+	D=A
+	M=D-1
+	@i
+	D=M //deincrements i so that the black goes away
 
-	@LOOP
+	@WHITE
 	D; JMP
 
 (END)
