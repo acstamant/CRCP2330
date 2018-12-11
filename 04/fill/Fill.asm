fdb3182b6214b41cf173e6e713f034e8c6660eb3
acstamant <astamant@smu.edu>
2018-12-11 11:46:39 -0600

    Not blackening the screen

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -16,8 +16,48 @@
 	//sets the stage for a C instruction designed to manipulate a certain data memory location, by setting  A to the address of that location 
 	//It sets the stage for a subsequent C instruction that specifies a jump, first by loading the address of the jump destination into the A regesiter 
 
+(BEGIN)
+	@SCREEN //refers to the screen location
+	D=A //sets D=Screen
+	@i //refers to some memory location
+	M=D //sets the memory of i to D (i=SCREEN)
 
-@SCREEN //refers to the screen location
-D=A //sets D=Screen
-@i //refers to some memory location
-M=D //sets the memory of i to D (i=SCREEN)
+
+(LOOP)
+	@KBD //refers to the keyboard location
+	D=M //sets memory of D=M[KBD]
+
+	@BLACK //refers to the BLACK loop
+	D; JNE //jumps D (which is set to the keyboard input) is equal to 1
+
+	@WHITE //refers to the WHITE loop
+	D; JEQ //jumps if D(which is set to the keyboard input) is equal to 0
+
+
+(BlACK)
+	@i //refers to the current memory address 
+	D=A//sets D to the current address 
+	M=D+1 //increments the memory to the next address
+
+	@SCREEN
+	M=-1 //sets the memory location to -1 
+	D=A //stores address in D
+	@i
+	A=D+1 //Increments the address by 1 
+
+	@LOOP//goes back to the loop
+	0;JMP
+
+(WHITE)
+	@i //refers to the current memory address 
+	D=A//sets D to the current address 
+	M=D+1 //increments the memory to the next address
+
+	@SCREEN
+	M=0//sets the memory location to -1 
+	D=A //stores address in D
+	@i
+	A=D-1 //DeIncrements the address by 1 
+
+	@LOOP //goes back to the loop
+	0;JMP
