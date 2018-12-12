12ee882bacb3ad165a0a2ee5b34c09b7dabadee2
acstamant <astamant@smu.edu>
2018-12-12 00:14:13 -0600

    still working in the symbol table, about to move to code

--- 06/symbolTable.cpp
+++ 06/symbolTable.cpp
@@ -4,93 +4,28 @@
 #include <map>
 using namespace std; 
 
+map<std::string, int> predefinedMap; //declares the map beforehand
+
 symbolTable::symbolTable() //default constructor
 {
 
 }
 
-
-void symbolTable::maps()
+void addEntry(std::string symbol, int address)//adds (symbol, address) to the table
 {
-	std::map<std::string, std::string> predefinedMap;
-	std::map<std::string, std::string> comp; 
-	std::map<std::string, std::string> dest;
-	std::map<std::string, std::string> jump; 
+	predefinedMap.insert(make_pair(symbol, address));
+}
 
-//PREDEFINED MAP
-	predefinedMap.emplace("SP", "0"); 
-	predefinedMap.emplace("LCL", "1"); 
-	predefinedMap.emplace("ARG", "2");
-	predefinedMap.emplace("THIS", "3"); 
-	predefinedMap.emplace("THAT", "4"); 
-	predefinedMap.emplace("R0", "0");
-	predefinedMap.emplace("R1", "1"); 
-	predefinedMap.emplace("R2", "2"); 
-	predefinedMap.emplace("R3", "3");
-	predefinedMap.emplace("R4", "4"); 
-	predefinedMap.emplace("R5", "5"); 
-	predefinedMap.emplace("R6", "6");
-	predefinedMap.emplace("R7", "7"); 
-	predefinedMap.emplace("R8", "8"); 
-	predefinedMap.emplace("R9", "9");
-	predefinedMap.emplace("R10", "10"); 
-	predefinedMap.emplace("R11", "11"); 
-	predefinedMap.emplace("R12", "12");
-	predefinedMap.emplace("R13", "13"); 
-	predefinedMap.emplace("R14", "14"); 
-	predefinedMap.emplace("R15", "5");
-	predefinedMap.emplace("SCREEN", "16384"); 
-	predefinedMap.emplace("KBD", "24576");
+bool contain(std::string symbol)//finds if the symbol is already in the map
+{
+	map<std::string, int>::iterator it=predefinedMap.find(symbol);  //searches for the symbol 
+	return it != predefinedMap.end() ? true : false;  //if it is not in map, return true, else return false
+}
 
-//COMP MAP
-	comp.emplace("0", "101010"); 
-	comp.emplace("1", "111111"); 
-	comp.emplace("-1", "111010");
-	comp.emplace("D", "001100"); 
-	comp.emplace("A", "110000"); 
-	comp.emplace("!D", "001101");
-	comp.emplace("!A", "110001"); 
-	comp.emplace("-D", "001111"); 
-	comp.emplace("-A", "110011");
-	comp.emplace("D+1", "011111"); 
-	comp.emplace("A+1", "110111"); 
-	comp.emplace("D-1", "001110");
-	comp.emplace("A-1", "110010"); 
-	comp.emplace("D+A", "000010"); 
-	comp.emplace("D-A", "010011");		
-	comp.emplace("A-D", "000111"); 
-	comp.emplace("D&A", "000000"); 
-	comp.emplace("D|A", "010101");
-
-	comp.emplace("M", "110000"); 
-	comp.emplace("!M", "110001"); 
-	comp.emplace("-M", "110011");
-	comp.emplace("M+1", "110111"); 
-	comp.emplace("M-1", "110010"); 
-	comp.emplace("D+M", "000010");
-	comp.emplace("D-M", "010011"); 
-	comp.emplace("M-D", "000111"); 
-	comp.emplace("D&M", "000000");
-	comp.emplace("D|M", "010101");
-
-//DEST MAP
-	dest.emplace("","000");
-	dest.emplace("M","001"); 
-	dest.emplace("D","010");
-	dest.emplace("MD","011");   
-	dest.emplace("A","100");
-	dest.emplace("AM","101"); 
-	dest.emplace("AD","110");
-	dest.emplace("AMD","111"); 
-
-//COMP MAP 
-	jump.emplace("","000");
-	jump.emplace("JGT","001");
-	jump.emplace("JEQ","010");
-	jump.emplace("JGE","011");   
-	jump.emplace("JLT","100");
-	jump.emplace("JNE","101");
-	jump.emplace("JLE","110");
-	jump.emplace("JMP","111"); 
-
+void display() //displays the map 
+{
+	for(auto mapItem : predefinedMap)
+	{
+		cout<<mapItem.first << '\t' << mapItem.second << endl;
+	}
 }
--- 06/symbolTable.h
+++ 06/symbolTable.h
@@ -3,24 +3,105 @@
 
 class symbolTable
 {
+	//initalizes the container
 public:
+	void maps()
+		{
+			map<std::string, int> predefinedMap;
+
+		//PREDEFINED MAP
+			predefinedMap.emplace("SP", 0); 
+			predefinedMap.emplace("LCL", 1); 
+			predefinedMap.emplace("ARG", 2);
+			predefinedMap.emplace("THIS", 3); 
+			predefinedMap.emplace("THAT", 4); 
+			predefinedMap.emplace("R0", 1);
+			predefinedMap.emplace("R1", 1); 
+			predefinedMap.emplace("R2", 2); 
+			predefinedMap.emplace("R3", 3);
+			predefinedMap.emplace("R4", 4); 
+			predefinedMap.emplace("R5", 5); 
+			predefinedMap.emplace("R6", 6);
+			predefinedMap.emplace("R7", 7); 
+			predefinedMap.emplace("R8", 8); 
+			predefinedMap.emplace("R9", 9);
+			predefinedMap.emplace("R10", 10); 
+			predefinedMap.emplace("R11", 11); 
+			predefinedMap.emplace("R12", 12);
+			predefinedMap.emplace("R13", 13); 
+			predefinedMap.emplace("R14", 14); 
+			predefinedMap.emplace("R15", 15);
+			predefinedMap.emplace("SCREEN", 16384); 
+			predefinedMap.emplace("KBD", 24576);
+
+	//COMP MAP
+		//WHEN A=0
+			predefinedMap.emplace("0", 101010); 
+			predefinedMap.emplace("1", 111111); 
+			predefinedMap.emplace("-1", 111010);
+			predefinedMap.emplace("D", 001100); 
+			predefinedMap.emplace("A", 110000); 
+			predefinedMap.emplace("!D", 001101);
+			predefinedMap.emplace("!A", 110001); 
+			predefinedMap.emplace("-D", 001111); 
+			predefinedMap.emplace("-A", 110011);
+			predefinedMap.emplace("D+1", 011111); 
+			predefinedMap.emplace("A+1", 110111); 
+			predefinedMap.emplace("D-1", 001110);
+			predefinedMap.emplace("A-1", 110010); 
+			predefinedMap.emplace("D+A", 000010); 
+			predefinedMap.emplace("D-A", 010011);		
+			predefinedMap.emplace("A-D", 000111); 
+			predefinedMap.emplace("D&A", 000000); 
+			predefinedMap.emplace("D|A", 010101);
+		//WHEN A=1
+			predefinedMap.emplace("M", 110000); 
+			predefinedMap.emplace("!M", 110001); 
+			predefinedMap.emplace("-M", 110011);
+			predefinedMap.emplace("M+1", 110111); 
+			predefinedMap.emplace("M-1", 110010); 
+			predefinedMap.emplace("D+M", 000010);
+			predefinedMap.emplace("D-M", 010011); 
+			predefinedMap.emplace("M-D", 000111); 
+			predefinedMap.emplace("D&M", 000000);
+			predefinedMap.emplace("D|M", 010101);
+
+	//DEST MAP
+			predefinedMap.emplace("",000);
+			predefinedMap.emplace("M",001); 
+			predefinedMap.emplace("D",010);
+			predefinedMap.emplace("MD",011);   
+			predefinedMap.emplace("A",100);
+			predefinedMap.emplace("AM",101); 
+			predefinedMap.emplace("AD",110);
+			predefinedMap.emplace("AMD",111); 
+
+	//COMP MAP 
+			predefinedMap.emplace("",000);
+			predefinedMap.emplace("JGT",001);
+			predefinedMap.emplace("JEQ",010);
+			predefinedMap.emplace("JGE",011);   
+			predefinedMap.emplace("JLT",100);
+			predefinedMap.emplace("JNE",101);
+			predefinedMap.emplace("JLE",110);
+			predefinedMap.emplace("JMP",111); 
+		}
 	//constructor that creates new table
 	symbolTable();
 	//Adds the pair (symbol, address) to the table
 	void addEntry(std::string symbol, int address);
-
+	
 	//Does the symbol table contain the given symbol? 
-	bool contrain(std::string symbol);
+	bool contain(std::string symbol);
 
 	//returns the address associated with the symbol 
 	int GetAddress(std::string symbol);
+	void display();
 
-	void maps();
+	
 
 	private:
 		std::string symbol; 
 		int address; 
-
-
 
 };
